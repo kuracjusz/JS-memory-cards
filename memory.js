6e6253bc535b713becb2c82038657e26dd400412
Witold Kosior <witold.kosior@pl.abb.com>
2017-09-20 16:42:38 +0200

    init

---
+++ memory.js
@@ -0,0 +1,6 @@
+var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iowerth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iowerth.png"];
+
+// alert(cards[4]);
+
+// console.log(cards);
+
